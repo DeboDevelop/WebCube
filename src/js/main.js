d7f8eb514d6986b17f5ec08ddd5eb074c1d1a011
DeboDevelop <debajyotidofficial@gmail.com>
2020-04-01 12:27:12 +0530

    Created the Orbit Controls

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
-let scene, camera, renderer, cube;
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r110/build/three.module.js';
+import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r110/examples/jsm/controls/OrbitControls.js';
+
+let scene, camera, renderer, cube, controls;
 
 function init()
 {
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
     renderer = new THREE.WebGLRenderer();
     renderer.setClearColor("#e5e5e5");
@@ -11,7 +14,10 @@ function init()
 
     document.body.appendChild(renderer.domElement);
 
-    camera.position.z = 400;
+    controls = new OrbitControls( camera, renderer.domElement );
+
+    camera.position.set( 0, 0, 400 );
+    controls.update();
 
     let geometry = new THREE.BoxGeometry( 200, 200, 200 );
     let material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
@@ -35,6 +41,8 @@ function onWindowResize() {
 function animate() {
 
     requestAnimationFrame( animate );
+
+    controls.update();
 
     cube.rotation.x += 0.005;
     cube.rotation.y += 0.01;
